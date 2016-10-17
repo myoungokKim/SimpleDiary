5aa43887d9b0cfd90926e134f99ad479349b6a94
Myoungok Kim <cheese0414@naver.com>
2016-10-17 20:55:44 +0900

    #2 DatePicker EventProgramming

--- app/src/main/java/kr/hs/emirim/cheese0414/simplediary/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/cheese0414/simplediary/MainActivity.java
@@ -2,12 +2,43 @@ package kr.hs.emirim.cheese0414.simplediary;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.*;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
-
+    DatePicker datePic;
+    EditText editDiary;
+    Button butSave;
+    String fileName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        datePic=(DatePicker)findViewById(R.id.date_picker);
+        editDiary=(EditText)findViewById(R.id.edit_content);
+        butSave=(Button)findViewById(R.id.but_save);
+
+//        현재 날짜 구하기
+        Calendar calendar=Calendar.getInstance();
+        int nowYear=calendar.get(Calendar.YEAR);
+        final int month=calendar.get(Calendar.MONTH)+1; //0은 1월을 반환하므로 +1을 해야한다.
+        int date=calendar.get(Calendar.DATE);
+
+//        DatePicker에 현재 날짜 설정
+        datePic.init(nowYear, month, date, new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                fileName=year+"_"+(monthOfYear+1)+"_"+dayOfMonth+".txt";
+                String content=readDiary(fileName);
+                editDiary.setText(content);
+                butSave.setEnabled(true); //저장할 수 있게끔 활성화시킴
+            }
+        });
+    }
+    String readDiary(String fileName) {
+        return null;
     }
 }
